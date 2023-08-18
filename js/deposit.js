dc346e755f737d3e6d4b85b104f087c67ef1d12c
khaled0915 <khaledsaifullah50956@gmail.com>
2023-08-18 15:49:45 +0600

    update the deposit and balance and handle string add

--- js/deposit.js
+++ js/deposit.js
@@ -7,13 +7,49 @@ document.getElementById('btn-deposit').addEventListener('click' , function(){
     // step-02 : get the deposit amount from the deosit input field
 
     const depositField = document.getElementById('Deposit-field');
-    const depositAmount = depositField.value ;
+    const NewDepositAmountString = depositField.value ;
+    const NewDepositAmount = parseFloat(NewDepositAmountString);
     // console.log(depositAmount);
 
     // step-03 : get the current deposit total 
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // step-04 : add numbers to set the total 
+
+    const currentDepositTotal = previousDepositTotal + NewDepositAmount;
+    
+    // set the deposit total
+    depositTotalElement.innerText = currentDepositTotal;
+
+
+    // step-05 : get balance current total 
+    const balanceTotalElement = document.getElementById('balane-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-06 : calculate current total balance
+
+    const currentBalanceTotal = previousBalanceTotal + NewDepositAmount ;
+
+
+    // set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+
+
+
+
+
+
+
+
+
+    // step - : clear the deposit field
+
+    depositField.value = ' ';
 
 
 })
